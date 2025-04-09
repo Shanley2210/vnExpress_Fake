225d6702007ad694bd4cbcfb5af1b624fe98d879
Shanley <nguyenhieushanley2210@gmail.com>
2025-04-09 22:02:56 +0700

    fix contrains

--- NodeJS/src/models/index.js
+++ NodeJS/src/models/index.js
@@ -7,26 +7,50 @@ const Comment = require('./comment.model');
 const Category = require('./category.model');
 const RefreshToken = require('./refreshToken.model');
 
-User.hasMany(Comment, { foreignKey: 'user_id', as: 'comments', onDelete: 'CASCADE' });
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    as: 'comments',
+    onDelete: 'CASCADE'
+});
 Comment.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 
-Article.hasMany(Comment, { foreignKey: 'article_id', as: 'comments', onDelete: 'CASCADE' });
+Article.hasMany(Comment, {
+    foreignKey: 'article_id',
+    as: 'comments',
+    onDelete: 'CASCADE'
+});
 Comment.belongsTo(Article, { foreignKey: 'article_id', as: 'article' });
 
-Comment.hasMany(Comment, { foreignKey: 'parent_id', as: 'replies', onDelete: 'CASCADE' });
+Comment.hasMany(Comment, {
+    foreignKey: 'parent_id',
+    as: 'replies',
+    onDelete: 'CASCADE'
+});
 Comment.belongsTo(Comment, { foreignKey: 'parent_id', as: 'parent' });
 
-User.hasMany(Article, { foreignKey: 'author_id', as: 'articles', onDelete: 'CASCADE' });  
-Article.belongsTo(User, { foreignKey: 'author_id', as: 'author' });  
+User.hasMany(Article, {
+    foreignKey: 'author_id',
+    as: 'articles',
+    onDelete: 'CASCADE'
+});
+Article.belongsTo(User, { foreignKey: 'author_id', as: 'author' });
 
-Category.hasMany(Article, {foreignKey: 'category_id', as: 'articles'});
-Article.belongsTo(Category, {foreignKey: 'category_id', as: 'category'});
+Category.hasMany(Article, { foreignKey: 'category_id', as: 'articles' });
+Article.belongsTo(Category, { foreignKey: 'category_id', as: 'category' });
 
-User.hasMany(RefreshToken, { foreignKey: 'user_id' , onDelete: 'CASCADE'});
+User.hasMany(RefreshToken, { foreignKey: 'user_id', onDelete: 'CASCADE' });
 RefreshToken.belongsTo(User, { foreignKey: 'user_id' });
 
-Category.hasMany(Category, { as: 'children', foreignKey: 'parent_id' });
-Category.belongsTo(Category, { as: 'parent', foreignKey: 'parent_id' });
+Category.hasMany(Category, {
+    as: 'children',
+    foreignKey: 'parent_id',
+    constraints: false
+});
+Category.belongsTo(Category, {
+    as: 'parent',
+    foreignKey: 'parent_id',
+    constraints: false
+});
 
 module.exports = {
     User,
