1c877181ff0790f6b2f3ec3c8c161e55d03e55a9
KaKaaaa2183 <khangdppk2183@gmail.com>
2025-04-28 21:11:01 +0700

    router moi

--- ReactJS/src/routers/router.js
+++ ReactJS/src/routers/router.js
@@ -1,29 +1,34 @@
-import { lazy } from 'react';
-import Admin from '@pages/Admin/Admin';
+import { lazy } from "react";
+import Admin from "@pages/Admin/Admin";
 
 const routers = [
-    {
-        path: '/',
-        component: lazy(() => import('@pages/HomePage/HomePage'))
-    },
-    {
-        path: '/login',
-        component: lazy(() => import('@pages/Login/Login'))
-    },
-    {
-        path: '/register',
-        component: lazy(() => import('@pages/Register/Register'))
-    },
-    { path: '/admin', component: Admin },
-    ,
-    {
-        path: '/callback',
-        component: lazy(() => import('@components/CallBack/Callback'))
-    },
-    {
-        path: '/detail/:slug',
-        component: lazy(() => import('@pages/Detail/Detail'))
-    }
+  {
+    path: "/",
+    component: lazy(() => import("@pages/HomePage/HomePage")),
+  },
+  {
+    path: "/login",
+    component: lazy(() => import("@pages/Login/Login")),
+  },
+  {
+    path: "/register",
+    component: lazy(() => import("@pages/Register/Register")),
+  },
+  { path: "/admin", component: Admin },
+  ,
+  {
+    path: "/callback",
+    component: lazy(() => import("@components/CallBack/Callback")),
+  },
+  {
+    path: "/detail/:slug",
+    component: lazy(() => import("@pages/Detail/Detail")),
+  },
+
+  {
+    path: "/category/:categoryId",
+    component: lazy(() => import("@pages/CategoryPage/CategoryPage")),
+  },
 ];
 
 export default routers;
